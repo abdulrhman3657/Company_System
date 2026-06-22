7c868d15d6cf51e645aa5d874697a9b61dab5261
abdulrhman3657 <138716960+abdulrhman3657@users.noreply.github.com>
2026-06-22 13:35:41 +0300

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { Employees } from "./employees/employees";
 import { Departments } from "./departments/departments";
+import { Home } from "./home/home";
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'employees', component: Employees },
     { path: 'departments', component: Departments}
 ]
