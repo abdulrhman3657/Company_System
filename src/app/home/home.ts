dba721b506ceda3a29abb350e29b171f2030dafb
abdulrhman3657 <138716960+abdulrhman3657@users.noreply.github.com>
2026-06-30 10:57:28 +0300

    feat: enhance home component with user details and employee/department counts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,47 @@
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { ChangeDetectorRef, Component, inject, OnInit } from "@angular/core";
+import { EmployeeApi } from "../services/employee-api";
+import { DepartmentApi } from "../services/department-api";
+import { AuthApi } from "../services/auth-api";
+
 
 @Component({
     selector: 'app-home',
-    imports: [RouterLink],
+    imports: [],
     templateUrl: './home.html',
     styleUrl: './home.css'
 })
 
-export class Home {
-    
+export class Home implements OnInit {
+    private employeeApi = inject(EmployeeApi);
+    private departmentApi = inject(DepartmentApi);
+    private authApi = inject(AuthApi);
+    private cdr = inject(ChangeDetectorRef)
+
+    username = '';
+    role = '';
+    employeeCount = 0;
+    departmentCount = 0;
+
+    ngOnInit() {
+        const user = this.authApi.getCurrentUserFromToken();
+
+        if(user) {
+            this.username = user.username;
+            this.role = user.role;
+        }
+
+        this.employeeApi.getEmployees().subscribe({
+            next: (response) => {
+                this.employeeCount = response.data?.length ?? 0;
+                this.cdr.detectChanges()
+            }
+        });
+
+        this.departmentApi.getDepartments().subscribe({
+            next: (response) => {
+                this.departmentCount = response.data?.length ?? 0;
+                this.cdr.detectChanges();
+            }
+        });
+    }
 }
